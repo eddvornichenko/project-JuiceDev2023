136428d8a83708cdffa30b36786abca00c339bd2
Bird-Forest <bird.forest70@gmail.com>
2023-05-21 13:49:20 +0300

    add modal window

---
+++ src/js/modal.js
@@ -0,0 +1,31 @@
+// Модальное окно ORDER
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// Модальное окно  THANK YOU
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[thank-modal-open]"),
+      closeModalBtn: document.querySelector("[thank-modal-close]"),
+      modal: document.querySelector("[thank-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
